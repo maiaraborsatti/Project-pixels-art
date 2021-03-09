f407ac6c2b623c01d2a6a19f231cbd256adb3f4d
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-09 20:27:57 -0300

    mudança no requisito 4 - criaçao de funçao

---
+++ script.js
@@ -0,0 +1,16 @@
+// Sugestão no plantão, função para criar o quadro de pixels
+function creatBoxes() {
+  let dad = document.getElementById('pixel-board');
+  for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 5; index++) {
+        let box = document.createElement('div');
+        box.className = 'pixel';
+        dad.appendChild(box);
+    }
+      let line = document.createElement('div');
+      line.className = 'line';
+      dad.appendChild(line);
+  }
+}
+creatBoxes()
+
