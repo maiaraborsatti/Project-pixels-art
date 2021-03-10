7b16cc545333ae12819792191e6ee690b3507778
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-10 16:54:13 -0300

    requisito 7

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function creatBoxes() {
 }
 creatBoxes()
 
+// Movendo a classe selected com um click
+// Entendimento de event.target: https://www.w3schools.com/jsref/event_target.asp
+// Entendimento classList.add e .remove: https://stackoverflow.com/questions/507138/how-to-add-a-class-to-a-given-element
+let boxBlack = document.getElementById('black');
+let boxYellow = document.getElementById('yellow');
+let boxBlue = document.getElementById('blue');
+let boxGreen = document.getElementById('green');
+
+boxBlack.addEventListener('click',changeClass);
+boxYellow.addEventListener('click',changeClass);
+boxBlue.addEventListener('click',changeClass);
+boxGreen.addEventListener('click',changeClass);
+
+function changeClass (event) {
+    let selectedColor = document.querySelector('.selected');
+    selectedColor.classList.remove('selected');
+    event.target.classList.add('selected');
+}
